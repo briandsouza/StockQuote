4146cb93f8d49371dc566b9d82917944fa75b8ff
Brian D'Souza <darksideofthemoon@gmail.com>
2019-04-23 22:27:16 +0800

    Removed some redundant code

--- GetData.py
+++ GetData.py
@@ -1,4 +1,3 @@
-#import mod_initialize
 import urllib3 as req
 import json
 import csv
@@ -7,42 +6,38 @@ import sys
 
 req.disable_warnings()
 
-def ConnectToService(apikey):
-    print (apikey) 
-    print ("Connecting")
-    conn_url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=DXC&apikey=' + apikey
-    print (conn_url)
+def ConnectToService(cmpname, apikey):
+    print ("-----------------------------Connecting---------------------------------------")
+    conn_url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + cmpname + '&apikey=' + apikey
     con = req.PoolManager()
     resp = con.request('GET', conn_url)
     soup = bs(resp.data, 'html.parser')
-    print ("received")
-    print (soup)
+    print ("---------------------------Quote Received----------------------------------------")
     resp.close()
     resp.release_conn()
     return soup
 
 def FormatData(soup):
-    print ("Formatting")
     js = json.loads(str(soup))
     js2 = js['Global Quote']
     write_array = []
     for x, y in js2.items():
         write_array.append(y)    
-    print(write_array)
     return write_array
 
 def AppendToFile(write_array):
-    print ("printing input")
-    print (write_array)
+    print ("---------------------Saving Output to File--------------------------")
     with open('StockQuotes.csv', 'a') as fd:
         writer = csv.writer(fd)
         writer.writerow(write_array)    
+    print ("-----------------------Finished Writing File------------------------")
 
 def CloseConToService(resp):
     resp.close()
 
 if __name__ == "__main__":
-    soup = ConnectToService(str(sys.argv[1]))
+    #To Do
+    #Need to sanitize the URL and arguments
+    soup = ConnectToService(str(sys.argv[1]),str(sys.argv[2]))
     aray = FormatData(soup)
     AppendToFile(aray)
-
